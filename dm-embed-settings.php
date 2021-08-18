246a175f5de56e7f0a11a4e114694a27302a8b11
Yudhi Satrio <isatrio.is.me@gmail.com>
2021-08-18 14:01:33 +0700

    fix: release stable version 1.0.1

--- dm-embed-settings.php
+++ dm-embed-settings.php
@@ -5,11 +5,11 @@
  * Description: Embed video from Dailymotion
  * Author: DMVS APAC Team
  * Author URI: https://github.com/DMVS-APAC
- * Version: 1.0.0-17
+ * Version: 1.0.1
  * Plugin URI: https://github.com/DMVS-APAC/wp-plugin-custom-embed
  * Download
  *
- * @version 1.0.0-17
+ * @version 1.0.1
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 require 'vendor/autoload.php';
 
-define( 'DM_CE__VERSION', '1.0.0-17');
+define( 'DM_CE__VERSION', '1.0.1');
 define( 'DM__FILE__', __FILE__ );
 define( 'DM__PLUGIN_BASE', plugin_basename( DM__FILE__ ) );
 define( 'DM__PATH', plugin_dir_path( DM__FILE__ ) );
