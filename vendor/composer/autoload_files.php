081d8a1ae8c10bcaca9f89bb8e767d2faebb290f
Yudhi Satrio <yudhi.satrio@dailymotion.com>
2022-02-08 15:26:01 +0700

    fix: run composer production package

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,7 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    '320cde22f66dd4f5d3fd621d3e88b98f' => $vendorDir . '/symfony/polyfill-ctype/bootstrap.php',
-    '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
     '49a1299791c25c6fd83542c6fedacddd' => $vendorDir . '/yahnis-elsts/plugin-update-checker/load-v4p11.php',
 );
