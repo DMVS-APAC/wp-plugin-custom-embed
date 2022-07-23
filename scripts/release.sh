fb31047af6cc60ab31083d903bf4ef7a8f8fd33b
Yudhi Satrio <yudhi.satrio@dailymotion.com>
2022-07-23 09:07:58 +0700

    chore: release to version 1.4.5

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,4 +4,10 @@ set -e
 npm run release
 npm run update-version
 npm run build:all
-git commit -am "create tag $1"
+
+CURRENT_VERSION=$(npm run version --silent)
+
+git tag -a v$CURRENT_VERSION -m "created tag v$CURRENT_VERSION"
+git commit -am "chore: release to version $CURRENT_VERSION"
+
+echo "Run \`git push --follow-tags origin $CURRENT_VERSION\`"
